a3d2ae32eb418da5433e58407d2c3c0d82036dee
jswap21 <jswap21@gmail.com>
2021-07-03 18:07:53 +0530

    colourful images

--- sketch.js
+++ sketch.js
@@ -77,11 +77,11 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
   
-    if((touches[].length>0)||keyDown("space") && trex.y >= 159) {
+   /* if((touches[].length>0)||keyDown("space") && trex.y >= 159) {
       trex.velocityY = -32;
       touches[]=[];
       
-    }
+    }*/
   
     trex.velocityY = trex.velocityY + 0.8
   
